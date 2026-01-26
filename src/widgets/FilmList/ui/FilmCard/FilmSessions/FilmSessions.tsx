69b43f07369a18a4e0612633caeb173b0dfb966e
NBhey <122431282+NBhey@users.noreply.github.com>
2026-01-26 21:05:26 +0300

    добавить дизейбл кнопкам сеанса, добавить переход на страницу брони сеанса

--- src/widgets/FilmList/ui/FilmCard/FilmSessions/FilmSessions.tsx
+++ src/widgets/FilmList/ui/FilmCard/FilmSessions/FilmSessions.tsx
@@ -1,10 +1,12 @@
 import { Seances } from '@/shared/api/type'
-import  styles  from './FilmSessions.module.css'
+import styles from './FilmSessions.module.css'
 import { Typography } from '@/shared/ui/Typography/Typography'
+import { NavLink } from 'react-router'
 
 type seanceIdField = Record<
   string,
   {
+    hallid: number
     seance_id: number[]
     seance_filmid: number
     seance_time: string[]
@@ -20,6 +22,7 @@ export const FilmSessions = ({ seances }: { seances: Array<Seances> }) => {
       seanceIdField[seance.seance_hallid].seance_time.push(seance.seance_time)
     } else {
       seanceIdField[seance.seance_hallid] = {
+        hallid: seance.seance_hallid,
         seance_id: [seance.id],
         seance_filmid: seance.seance_filmid,
         seance_time: [seance.seance_time],
@@ -28,21 +31,36 @@ export const FilmSessions = ({ seances }: { seances: Array<Seances> }) => {
   })
 
   console.log(seanceIdField)
-  console.log(Object.keys(seanceIdField))
 
   return (
     <div>
       {Object.keys(seanceIdField).map((seance, numberHall) => {
-        console.log(seance)
         return (
-          <div>
-            <Typography as='p' variant='heading-sm' className={styles.hall}>{`Зал ${numberHall + 1}`}</Typography>
+          <div key={numberHall}>
+            <Typography as="p" variant="heading-sm" className={styles.hall}>
+              {`Зал ${numberHall + 1}`}
+            </Typography>
+
             <div className={styles.time_row}>
-              {seanceIdField[seance].seance_time.map((time) => {
-              return <span className={styles.time}>{time}</span>
-            })}
+              {seanceIdField[seance].seance_time.map((time, position) => {
+                if (Number(time.slice(0, 2)) < new Date().getHours()) {
+                  return (
+                    <span key={time} className={styles.time_disabled}>
+                      {time}
+                    </span>
+                  )
+                }
+                return (
+                  <NavLink
+                    key={time}
+                    to={`/halls/${seanceIdField[seance].hallid}/seances/${seanceIdField[seance].seance_id[position]}`}
+                    className={styles.time}
+                  >
+                    {time}
+                  </NavLink>
+                )
+              })}
             </div>
-            
           </div>
         )
       })}
