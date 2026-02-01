3715a8dc6ddf2e79a996f510b69ce7cc7ec332fc
NBhey <122431282+NBhey@users.noreply.github.com>
2026-02-01 21:45:00 +0300

    добавить линку на лого

--- src/widgets/Header/ui/TitleBlock/TitleBlock.tsx
+++ src/widgets/Header/ui/TitleBlock/TitleBlock.tsx
@@ -2,11 +2,14 @@ import { Button } from '@/shared/ui/Button/Button'
 import styles from './style.module.css'
 
 import { BrandTitle } from '@/shared/ui'
+import { Link } from 'react-router'
 
 export const TitleBlock = () => {
   return (
     <div className={styles.title}>
-      <BrandTitle />
+      <Link to="/">
+        <BrandTitle />
+      </Link>
 
       <Button text="войти" variant="enter" />
     </div>
