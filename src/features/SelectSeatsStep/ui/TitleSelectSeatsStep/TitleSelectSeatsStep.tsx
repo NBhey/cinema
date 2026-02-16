505b1634f6613ae58df4a62343a03d23923681db
NBhey <122431282+NBhey@users.noreply.github.com>
2026-02-16 19:16:54 +0300

    refactoring: вынести титульный лист зала из компонента select-seats-step

---
+++ src/features/SelectSeatsStep/ui/TitleSelectSeatsStep/TitleSelectSeatsStep.tsx
@@ -0,0 +1,58 @@
+import { Typography } from '@/shared/ui/Typography/Typography'
+import Skeleton from 'react-loading-skeleton'
+import styles from './TitleSelectSeatsStep.module.css'
+import { Films } from '@/shared/api/type'
+
+interface TitleSelectSeatsStep {
+  film: Films | undefined
+  seanceData: {
+    id: number
+    seanceFilmid: number
+    seanceHallid: number
+    seanceTime: string
+  } | undefined
+  hallName: string | undefined
+}
+
+export const TitleSelectSeatsStep: React.FC<TitleSelectSeatsStep> = ({
+  film,
+  seanceData,
+  hallName,
+}) => {
+  return (
+    <div className={styles['title']}>
+      <Typography variant="heading-sm" as="h3">
+        {film?.filmName || (
+          <Skeleton
+            count={1}
+            inline
+            width={90}
+            baseColor="#de9741"
+            highlightColor="#e6be89"
+            duration={0.35}
+          />
+        )}
+      </Typography>
+      <Typography variant="text-medium" as="p">
+        Начало сеанса:{' '}
+        {seanceData?.seanceTime || (
+          <Skeleton
+            count={1}
+            inline
+            width={45}
+            baseColor="#de9741"
+            highlightColor="#e6be89"
+            duration={0.35}
+          />
+        )}
+      </Typography>
+      <Typography
+        style={{ textTransform: 'capitalize' }}
+        as={'h4'}
+        variant="heading-sm"
+      >
+        {hallName}
+      </Typography>
+    </div>
+  )
+}
