cd2308ac5556e4f54f182458420fee42c05c9759
NBhey <122431282+NBhey@users.noreply.github.com>
2026-02-18 18:42:50 +0300

    refactoring: убрать обертку AppProvider, вынести background на уровень html

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,29 @@
 import './app/global.css'
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { MainPage } from './pages/MainPage/MainPage'
-
+import 'react-loading-skeleton/dist/skeleton.css'
 import { BookingLayout } from '@/widgets/Booking/ui/BookingLayout'
 import { SelectSeatsStep } from './features/SelectSeatsStep'
 import { ConfirmStep } from './features/ConfirmStep'
-import { AppProvider } from './shared/model/app-context/AppContext'
 import { MainPageLayout } from './pages/MainPage/ui/layout/MainPageLayout'
 
 function App() {
-  /* TODO неверно оборачиваю роуты, AppProvider не должен так оборачивать их  
-   создать отдельный лайаут под PublicLayout, так будет проще дальше работать с AdminLayout*/
   return (
     <Router>
-      <AppProvider>
-        <Routes>
-          <Route path="/" element={<MainPageLayout />}>
-            <Route index element={<MainPage />} />
-            <Route path=":date" element={<MainPage />} />
-          </Route>
+      <Routes>
+        <Route path="/" element={<MainPageLayout />}>
+          <Route index element={<MainPage />} />
+          <Route path=":date" element={<MainPage />} />
+
           <Route
-            path="/:date/halls/:hallName/seances/:seanceId"
+            path=":date/halls/:hallName/seances/:seanceId"
             element={<BookingLayout />}
           >
             <Route index element={<SelectSeatsStep />} />
             <Route path="confirm" element={<ConfirmStep />} />
           </Route>
-        </Routes>
-      </AppProvider>
+        </Route>
+      </Routes>
     </Router>
   )
 }
