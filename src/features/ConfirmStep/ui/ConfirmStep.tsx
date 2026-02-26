714bebfd8afa9e76b98ab5c7b6fdfdceeebf9371
NBhey <122431282+NBhey@users.noreply.github.com>
2026-02-26 19:17:02 +0300

    feat: добавить корешок странице согласия на бронирование

--- src/features/ConfirmStep/ui/ConfirmStep.tsx
+++ src/features/ConfirmStep/ui/ConfirmStep.tsx
@@ -1,7 +1,24 @@
+import { Typography } from '@/shared/ui/Typography/Typography'
+import styles from './ConfirmStep.module.css'
+import { useParams } from 'react-router-dom'
+import { useSeanceFilm } from '@/features/SelectSeatsStep/lib/useSeanceFilm'
+
 export const ConfirmStep = () => {
+  const { date, hallName, seanceId } = useParams()
+  const { film, seanceData, halls } = useSeanceFilm(seanceId as string)
+
   return (
-    <section>
-      <h1>Confirm</h1>
+    <section className={styles['wrapper']}>
+      <div className={styles['headerConfirm']}>
+        <Typography
+          className={styles['titleConfirm']}
+          as="h2"
+          variant="heading-bolt"
+        >
+          Вы выбрали билеты
+        </Typography>
+      </div>
+      <div className={styles['bodyConfirm']}></div>
     </section>
   )
 }
