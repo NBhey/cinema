04e12076222a17a9301708a75ce8870a01c8581c
NBhey <122431282+NBhey@users.noreply.github.com>
2026-02-28 16:33:49 +0300

    feat: добавить инфомрацию о выбранном фильме

--- src/features/ConfirmStep/ui/ConfirmStep.tsx
+++ src/features/ConfirmStep/ui/ConfirmStep.tsx
@@ -7,18 +7,52 @@ export const ConfirmStep = () => {
   const { date, hallName, seanceId } = useParams()
   const { film, seanceData, halls } = useSeanceFilm(seanceId as string)
 
+  const selectedSeatsString = localStorage.getItem('selectedSeats')
+  const selectedSeatsInfo: {
+    date: string
+    seats: Array<{ ряд: number; место: number }>
+  } = selectedSeatsString ? JSON.parse(selectedSeatsString) : null
+
+  const selectedSeats = selectedSeatsInfo?.seats.map((seat) => seat.место)
+
+  console.log('selectedSeats', seanceData)
   return (
     <section className={styles['wrapper']}>
       <div className={styles['headerConfirm']}>
         <Typography
           className={styles['titleConfirm']}
           as="h2"
-          variant="heading-bolt"
+          variant="heading-bold"
         >
           Вы выбрали билеты
         </Typography>
       </div>
-      <div className={styles['bodyConfirm']}></div>
+      <section className={styles['bodyConfirm']}>
+        <Typography as="p" variant="text-regular">
+          На фильм:{' '}
+          <Typography as="span" variant="heading-sm">
+            {film?.filmName}
+          </Typography>
+        </Typography>
+        <Typography as="p" variant="text-regular">
+          Места:{' '}
+          <Typography as="span" variant="heading-sm">
+            {selectedSeats.join(',')}
+          </Typography>
+        </Typography>
+        <Typography as="p" variant="text-regular">
+          В зале:{' '}
+          <Typography as="span" variant="heading-sm">
+            {hallName}
+          </Typography>
+        </Typography>
+        <Typography as="p" variant="text-regular">
+          Начало сеанса:{' '}
+          <Typography as="span" variant="heading-sm">
+            {seanceData?.seanceTime}
+          </Typography>
+        </Typography>
+      </section>
     </section>
   )
 }
