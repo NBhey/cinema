17ea85d33f301302472955a645a877dbb0fba72f
NBhey <122431282+NBhey@users.noreply.github.com>
2026-03-09 14:53:16 +0300

    feat: сверстать форму

--- src/pages/Autorization/Authorization.tsx
+++ src/pages/Autorization/Authorization.tsx
@@ -1,16 +1,55 @@
 import { BrandTitle } from '@/shared/ui'
 import { Typography } from '@/shared/ui/Typography/Typography'
 import styles from './Authorization.module.css'
+import { Button } from '@/shared/ui/Button/Button'
 
 export const Authorization = () => {
   return (
-    <header className={styles['header']}>
-      <div>
+    <>
+      <header className={styles['header']}>
         <BrandTitle />
         <Typography className={styles['subtitle']} as="p" variant="text-light">
           Администраторская
         </Typography>
-      </div>
-    </header>
+      </header>
+
+      <form className={styles['authorization-form']}>
+        <Typography
+          as="h5"
+          variant="heading-bold"
+          className={styles['authorization-form__title']}
+        >
+          Авторизация
+        </Typography>
+        <div className={styles['authorization-form__fields']}>
+          <p className={styles['authorization-form__field']}>
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              placeholder="example@domain.xyz"
+              autoComplete="email"
+            />
+          </p>
+          <p className={styles['authorization-form__field']}>
+            <label htmlFor="password">Пароль</label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+            />
+          </p>
+
+          <Button
+            className={styles['authorization-form__btn']}
+            text="Авторизоваться"
+            variant="enter"
+            type="submit"
+          ></Button>
+        </div>
+      </form>
+    </>
   )
 }
