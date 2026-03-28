b7d6ad728a8d71c8eb3047b11bc1ec416b8684e4
NBhey <122431282+NBhey@users.noreply.github.com>
2026-03-28 20:15:41 +0300

    feat: добавить инпуты в конфигурацию холла

--- src/features/ConfigurateHall/ConfigurateHall.tsx
+++ src/features/ConfigurateHall/ConfigurateHall.tsx
@@ -1,12 +1,37 @@
+import { PanelBodyWrapper } from '@/shared/ui'
 import useAdminPanelHeader from '../admin/lib/useAdminPanelHeader'
+import { useHallsQuery } from '@/shared/api/hall/quieries'
+import { useEffect, useState } from 'react'
+import { Hall } from '@/shared/api/type'
+import { ButtonHallList } from '../admin/ui/ButtonHallList/ButtonHallList'
+import { ConfigurateRowAndPlace } from '../admin/ui/ConfigurateRowAndPlace/ConfigurateRowAndPlace'
 
 export const ConfigurateHall = () => {
-  const { isOpenPanel, Header } = useAdminPanelHeader('Конфигурация залов')
+  const [activeHallId, setActiveHallId] = useState<Hall | null>(null)
 
+  const { isPanelOpen, Header } = useAdminPanelHeader('Конфигурация залов')
+  const { data } = useHallsQuery()
+  const halls = data?.result.halls
+
+  useEffect(() => {
+    if (halls) {
+      setActiveHallId(halls[0])
+    }
+  }, [halls])
+
+  console.log(activeHallId)
   return (
     <>
       <Header />
-      {isOpenPanel && <section>123</section>}
+      {isPanelOpen && (
+        <PanelBodyWrapper>
+          <ButtonHallList halls={halls} changeHall={setActiveHallId} />
+          <ConfigurateRowAndPlace
+            row={activeHallId?.hallRows}
+            places={activeHallId?.hallPlaces}
+          />
+        </PanelBodyWrapper>
+      )}
     </>
   )
 }
